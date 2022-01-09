116037aa546bd2e6483f3420cfb818ea85b308c0
douglasmeirelestc <88891330+douglasmeirelestc@users.noreply.github.com>
2022-01-09 18:18:21 -0300

    Alteração maximized chrome

--- capybarateste/tests/features/support/env.rb
+++ capybarateste/tests/features/support/env.rb
@@ -1,17 +1,14 @@
 require 'capybara/cucumber'
 require 'selenium-webdriver'
-require 'webdrivers'
-
+# require 'webdrivers'
 
 Capybara.register_driver :selenium_chrome do |app|
-    Capybara::Selenium::Driver.new(app, :browser => :chrome)
-  end
-
-
-
-Capybara.configure do |config|
-    config.default_driver = :selenium_chrome
-    config.app_host = 'https://automacaocombatista.herokuapp.com'
-    config.default_max_wait_time = 5
+  caps = Selenium::WebDriver::Remote::Capabilities.chrome("goog:chromeOptions" => {"args" => [ "--start-maximized" ]})
+  Capybara::Selenium::Driver.new(app, {:browser => :chrome, :desired_capabilities => caps})
 end
 
+Capybara.configure do |config|
+  config.default_driver = :selenium_chrome
+  config.app_host = 'https://automacaocombatista.herokuapp.com'
+  config.default_max_wait_time = 5
+end
